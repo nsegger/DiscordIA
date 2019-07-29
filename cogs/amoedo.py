23c3123467a5f2638a9ab791b1ba8fba889b978e
unknown <painn5@yahoo.com>
2019-07-29 15:19:04 -0300

    Amoedo image generator added. Thank you @margato.

--- cogs/amoedo.py
+++ cogs/amoedo.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from io import BytesIO
-import base64
+from base64 import b64decode
 from amoedogen.Generator import Generator
 import requests
 
@@ -15,7 +15,7 @@ class Amoedo(commands.Cog):
         gen = Generator()
         gen.write(text, color="blue", rect=True, base_64=True)
         img64 = gen.result
-        await ctx.send(file=discord.File(base64.decodestring(img64), f"a{ctx.author}.jpeg"))
+        await ctx.send(file=discord.File(BytesIO(b64decode(img64)), f"a{ctx.author}.jpeg"))
 
 def setup(client):
     client.add_cog(Amoedo(client))
