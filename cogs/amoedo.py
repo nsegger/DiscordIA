2fa804a37d0c535e1f7c6f644827ce9d6c869cf3
unknown <painn5@yahoo.com>
2019-07-29 13:24:02 -0300

    Amoedo image gen. command added.

---
+++ cogs/amoedo.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from config import amoedoURL
+import base64
+import requests
+
+class Amoedo(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def amoedo(self, ctx, *, text):
+        payload = {"text": text}
+        img64 = requests.post(amoedoURL, data=payload)
+        if img64.status_code == 200:
+            imgdata = base64.b64decode(img64)
+            filename = f'{ctx.author}.jpg'
+            with open(filename, 'wb') as image:
+                image.write(imgdata)
+                await self.client.send_file(ctx.channel, image)
+                image.close()
+        else:
+            await ctx.send(f'Não pude gerar a imagem:\n```STATUS: {img64.status_code}```')
+
+def setup(client):
+    client.add_cog(Amoedo(client))
+    print("Amoedo loaded!")
+
+def teardown(client):
+    print("Amoedo unloaded!")
