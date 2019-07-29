b43db87f42197fc2ccf7a7c20e41cab933d7fa38
unknown <painn5@yahoo.com>
2019-07-29 16:01:42 -0300

    I'm dumb, sorry.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from itertools import cycle
 
-token = "NTYxMzAyMzAwMDE0OTM2MDc2.XT8TCQ.bKaxMXEPfgwCIKBSmr_vL2krbvE"
+token = ""
 status = cycle(["sua mãe na cama", "ayyy lmao", "seu pai eh nosso", "não eh mole"])
 path = "cogs"
 
